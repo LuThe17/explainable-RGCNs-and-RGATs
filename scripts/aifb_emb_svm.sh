e3be8c59edb084762f8b481c19961b37a247ce95
Luisa Theobald <luisa.thebald@yahoo.de>
2023-09-03 15:34:09 +0200

    remove old files

--- scripts/aifb_emb_svm.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH --partition=gpu_4
-#SBATCH --gres=gpu:1
-#SBATCH --nodes=1
-#SBATCH --time=2:00:00
-#SBATCH --export=NONE
-
-python aifb_emb_svm.py
--- scripts/bgs_emb_single.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH --partition=single
-#SBATCH --nodes=1
-#SBATCH --time=14:00:00
-#SBATCH --export=NONE
-#SBATCH --mem=180000mb
-
-python aifb_emb.py
