d25a3684ed761b053e0b623cc8db26096c3165ee
Luisa Theobald <luisa.thebald@yahoo.de>
2023-09-04 00:04:27 +0200

    update files

--- env/train_gpu_lrp_rgcn_no_emb.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --partition=gpu_4
-#SBATCH --gres=gpu:1
-#SBATCH --nodes=1
-#SBATCH --time=5:00:00
-#SBATCH --export=NONE
-#SBATCH --mem-per-gpu=376000
-
-python train_gpu_lrp_rgcn_no_emb.py
--- env/train_gpu_lrp_rgcn_no_emb_8.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --partition=gpu_8
-#SBATCH --gres=gpu:1
-#SBATCH --nodes=1
-#SBATCH --time=5:00:00
-#SBATCH --export=NONE
-#SBATCH --mem=752000
-
-python train_gpu_lrp_rgcn_no_emb.py
